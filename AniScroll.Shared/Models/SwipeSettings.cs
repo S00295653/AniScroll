61bc8f55220aa5e0e8cde29bd4ac09a073a7fc38
FlyPhoenix52 <S00295653@atu.ie>
2026-03-13 17:49:37 +0000

    I WANT TO BREAK FREEE

---
+++ AniScroll.Shared/Models/SwipeSettings.cs
@@ -0,0 +1,11 @@
+﻿// AniScroll.Shared/Models/SwipeSettings.cs
+namespace AniScroll.Shared.Models;
+
+public class SwipeSettings
+{
+    /// <summary>Status applied when swiping RIGHT (default: Completed)</summary>
+    public ListStatus RightStatus { get; set; } = ListStatus.Completed;
+
+    /// <summary>Status applied when swiping LEFT (default: Planning)</summary>
+    public ListStatus LeftStatus { get; set; } = ListStatus.Planning;
+}
